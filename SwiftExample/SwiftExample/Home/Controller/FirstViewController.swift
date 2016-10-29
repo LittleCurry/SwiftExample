4bbec97827dd78601394a74f7c434a4148d8f9c4
李云鹏 <2441611062@qq.com>
2016-10-29 12:06:39 +0800

    ape展开

--- SwiftExample/SwiftExample/Home/Controller/FirstViewController.swift
+++ SwiftExample/SwiftExample/Home/Controller/FirstViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import AVFoundation
 
 class FirstViewController: BaseViewController, UITableViewDelegate, UITableViewDataSource{
-    // 这是写属性的地方
+    // 这是写属性的地方 
     var nameArr = ["navigationBar渐变", "聊天界面", "gauss模糊", "Share", "Map", "二维码", "视频播放", "block", "天气", "清除缓存", "日期选择", "我的银行卡", "本地相册选取", "轮播图", "热更新", "弹幕", "日历", "商城首页", "商城分类", "商城发现", "商城购物车", "商城我的", "请输入密码", "圆形菜单", "球形标签", "推荐影片", "渐隐文字", "打印文字", "拼图", "2048", "浏览卡片", "抽卡效果", "抖动菜单", "渐变色", "折卡效果", "卡牌拖动", "摇一摇", "ape展开", "余额跳动", "水平滚动布局", "发散菜单", "漂浮的雪花", "水波"];
     var myTableView = UITableView.init(frame: CGRectMake(0, 0, WIDTH, HEIGHT), style: UITableViewStyle.Plain);
     var clearLabel = UILabel.init(frame: CGRectMake(WIDTH-115, 450, 100, 50))
